e0f1aa787d3c0ba1eec59e77d4f3db19390cf08a
myla steiger <mrs.steiger@gmail.com>
2022-02-08 12:26:41 -0500

    Add project files.

---
+++ ProductInheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductInheritance {
+    class Program {
+        static void Main(string[] args) {
+
+            
+
+            List<Product> salesOrder = new List<Product>();
+            Product widget1 = new Widget("W1", 50, "small", "red");
+            Product widget2 = new Widget("W2", 100, "medium", "white");
+            Product widget3 = new Widget("W3", 200, "Large", "Blue");
+            Product service = new Service("SVC", 500, "Premium", 5);
+            salesOrder.Add(widget1);
+            salesOrder.Add(widget2);
+            salesOrder.Add(widget3);
+            salesOrder.Add(service);
+
+            decimal salesOrderTotal = 0;
+
+            foreach (Product p in salesOrder) {
+            salesOrderTotal += p.Price;
+                if (p is Widget) {
+                    Widget w = p as Widget;
+                    Console.WriteLine($"{w.Code} {w.Price:c} {w.Size}, {w.Color}");
+                    continue;
+                }
+                if (p is Service) {
+                    Service s = p as Service;
+                    Console.WriteLine($"{s.Code}, {s.Price}, {s.ServiceLevel}, {s.NumberofYears}");
+                    continue;
+                } 
+            }
+            Console.WriteLine($"Total sales order is {salesOrderTotal:c}");
+
+
+           /* Service s1 = new Service("Basic", 100, "Basic", 3);
+            Console.WriteLine(s1.ToPrint());
+
+            Widget w = new Widget("Widget",543.21m, "Large", "MAX Red");
+            w.Code = "Widget2";
+            w.Price = 543.32m;
+            Console.WriteLine(w.ToPrint());
+
+            Product p = new Product("Echo", 123.45m);
+            string message = p.ToPrint();
+            Console.WriteLine(message);*/
+        }
+    }
+}
---
+++ ProductInheritance/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInheritance {
+    public class Service : Product {
+
+        public string ServiceLevel { get; set; } = "Basic";
+        public int NumberofYears { get; set; } = 1;
+
+        public override string ToPrint() {
+            return $"{base.ToPrint()} | {ServiceLevel} | {NumberofYears}";
+        }
+
+        public Service(string code, decimal price, string serviceLevel, int numberofYears) 
+            : base(code, price){
+            ServiceLevel = serviceLevel;
+            NumberofYears = numberofYears;
+
+        }
+    }
+}
